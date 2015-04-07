9e6de343bd18c73d6ace999fa3817ce3f6376a3c
Tim Officer <timothyofficer@gmail.com>
2015-04-07 14:46:17 -0400

    application created notifications

--- src/modules/project/notificationService.js
+++ src/modules/project/notificationService.js
@@ -10,6 +10,8 @@ var errors = require('modules/error');
 var logger = require('modules/logger');
 var userService = require('modules/user');
 var projectApplicationService = require('modules/project/applicationService');
+var projectUserService = require('modules/project/userService');
+var permissionService = require('modules/permission');
 
 //models
 var User = require('modules/user/data/model');
@@ -18,10 +20,12 @@ var ProjectUser = require('modules/project/data/userModel');
 
 //libs
 var mailgunApi = require('lib/mailgun-api');
+var templateService = require('lib/services/templateService');
 
 /* =========================================================================
  * Constants
  * ========================================================================= */
+var PERMISSION_NAMES = require('modules/permission/constants/permissionNames');
 
 /* =========================================================================
  * Constructor
@@ -50,17 +54,47 @@ ProjectNotificationService.prototype.sendApplicationCreateNotifications = functi
     },
     project: function findProjectById_step(done) {
       Project.findById(options.projectId, done);
+    },
+    projectUsers: function findProjectUsersByProjectId_step(done) {
+      projectUserService.getByProjectId({
+        projectId: options.projectId
+      }, done);
+    },
+    addProjectUsersPermission: function findPermissionById_step(done) {
+      permissionService.getByName({
+        name: PERMISSION_NAMES.ADD_PROJECT_USERS
+      }, done);
     }
   }, function(err, results) {
     if (err) return next(err);
 
     var project = results.project;
+    var projectUsers = results.projectUsers;
     var projectApplication = results.projectApplication;
     var user = results.user;
+    var addProjectUsersPermission = results.addProjectUsersPermission;
 
     if (!project) return done(new errors.ObjectNotFoundError('Project not found'));
 
-    next();
+    async.waterfall([
+      function generateEmailTemplate_step(done) {
+        templateService.generate({
+          templateFilePath: __dirname + '/notifications/applicationCreated/email.html',
+          templateData: {
+            user: user,
+            project: project,
+            projectApplication: projectApplication
+          }
+        }, done);
+      },
+      function sendNotifications_step(emailText, done) {
+        var projectUsers = _.filter(projectUsers, function(projectUser) {
+          var hasPermission = _.contains(projectUser.permission, addProjectUsersPermission._id);
+        });
+
+        
+      }
+    ], next);
   });
 };
 
