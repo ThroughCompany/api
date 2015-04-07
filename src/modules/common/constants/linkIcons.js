6123b2033fe7e49b457037de9a73460d3ba17eab
Tim Officer <timothyofficer@gmail.com>
2015-04-07 14:46:01 -0400

    add link icons

---
+++ src/modules/common/constants/linkIcons.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ICON_FACEBOOK: 'icon-facebook',
+  ICON_GITHUB: 'icon-github',
+  ICON_LINKEDIN: 'icon-linkedin',
+  ICON_TWITTER: 'icon_twitter',
+  ICON_MEETUP: 'icon_meetup',
+  ICON_DRIBBBLE: 'icon-dribbble',
+  ICON_BEHANCE: 'icon-behance',
+  ICON_YELP: 'icon-yelp',
+  ICON_GOODREADS: 'icon-goodreads',
+  ICON_INSTAGRAM: 'icon-instagram',
+  ICON_WEBSITE: 'icon-website',
+  ICON_MEDIUM: 'icon-medium',
+  ICON_BLOG: 'icon-blog'
+};
