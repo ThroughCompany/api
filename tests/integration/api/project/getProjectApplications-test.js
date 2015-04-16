2fcf13e4ab8dd1db9aa36b7ff15e8ec879b4f892
Tim Officer <timothyofficer@gmail.com>
2015-04-16 12:39:07 -0400

    get project application tests

--- tests/integration/api/project/getProjectApplications-test.js
+++ tests/integration/api/project/getProjectApplications-test.js
@@ -20,6 +20,7 @@ var User = require('modules/user/data/model');
 var Admin = require('modules/admin/data/model');
 var Auth = require('modules/auth/data/model');
 var Project = require('modules/project/data/projectModel');
+var ProjectApplication = require('modules/project/data/applicationModel');
 
 var agent;
 
@@ -56,107 +57,222 @@ describe('api', function() {
         });
       });
 
-      // describe('when trying to update a project they aren\'t a member of and is not an admin', function() {
-      //   var email1 = 'testuser1@test.com';
-      //   var email2 = 'testuser2@test.com';
-      //   var password = 'password';
-      //   var user1 = null;
-      //   var user2 = null;
-      //   var auth = null;
-      //   var project = null;
+      describe('when trying to get applications for a project they aren\'t a member of and is not an admin', function() {
+        var email1 = 'testuser1@test.com';
+        var email2 = 'testuser2@test.com';
+        var password = 'password';
+        var user1 = null;
+        var user2 = null;
+        var auth = null;
+        var project = null;
 
-      //   before(function(done) {
-      //     async.series([
-      //       function createUser1_step(cb) {
-      //         userService.createUsingCredentials({
-      //           email: email1,
-      //           password: password
-      //         }, function(err, _user) {
-      //           if (err) return cb(err);
+        before(function(done) {
+          async.series([
+            function createUser1_step(cb) {
+              userService.createUsingCredentials({
+                email: email1,
+                password: password
+              }, function(err, _user) {
+                if (err) return cb(err);
 
-      //           user1 = _user;
-      //           cb();
-      //         });
-      //       },
-      //       function createUser2_step(cb) {
-      //         userService.createUsingCredentials({
-      //           email: email2,
-      //           password: password
-      //         }, function(err, _user) {
-      //           if (err) return cb(err);
+                user1 = _user;
+                cb();
+              });
+            },
+            function createUser2_step(cb) {
+              userService.createUsingCredentials({
+                email: email2,
+                password: password
+              }, function(err, _user) {
+                if (err) return cb(err);
 
-      //           user2 = _user;
-      //           cb();
-      //         });
-      //       },
-      //       function authenticateUser_step(cb) {
-      //         authService.authenticateCredentials({
-      //           email: email1,
-      //           password: password
-      //         }, function(err, _auth) {
-      //           if (err) return cb(err);
+                user2 = _user;
+                cb();
+              });
+            },
+            function authenticateUser_step(cb) {
+              authService.authenticateCredentials({
+                email: email1,
+                password: password
+              }, function(err, _auth) {
+                if (err) return cb(err);
 
-      //           auth = _auth;
-      //           cb();
-      //         });
-      //       },
-      //       function createUserProjectStep_step(cb) {
-      //         projectService.create({
-      //           createdByUserId: user2._id,
-      //           name: 'Project 1',
-      //           shortDescription: 'short desc'
-      //         }, function(err, _project) {
-      //           if (err) return cb(err);
+                auth = _auth;
+                cb();
+              });
+            },
+            function createUserProjectStep_step(cb) {
+              projectService.create({
+                createdByUserId: user2._id,
+                name: 'Project 1',
+                shortDescription: 'short desc'
+              }, function(err, _project) {
+                if (err) return cb(err);
 
-      //           project = _project;
-      //           cb();
-      //         });
-      //       }
-      //     ], done);
-      //   });
+                project = _project;
+                cb();
+              });
+            }
+          ], done);
+        });
 
-      //   after(function(done) {
-      //     User.remove({
-      //       email: {
-      //         $in: [email1, email2]
-      //       }
-      //     }, done);
-      //   });
+        after(function(done) {
+          User.remove({
+            email: {
+              $in: [email1, email2]
+            }
+          }, done);
+        });
 
-      //   after(function(done) {
-      //     Auth.remove({
-      //       user: user1._id
-      //     }, done);
-      //   });
+        after(function(done) {
+          Auth.remove({
+            user: user1._id
+          }, done);
+        });
 
-      //   after(function(done) {
-      //     Project.remove({
-      //       _id: project._id
-      //     }, done);
-      //   });
+        after(function(done) {
+          Project.remove({
+            _id: project._id
+          }, done);
+        });
 
-      //   it('should return a 403', function(done) {
-      //     agent
-      //       .patch('/projects/' + project._id)
-      //       .set('x-access-token', auth.token)
-      //       .end(function(err, response) {
-      //         should.not.exist(err);
+        it('should return a 403', function(done) {
+          agent
+            .get('/projects/' + project._id + '/applications')
+            .set('x-access-token', auth.token)
+            .end(function(err, response) {
+              should.not.exist(err);
 
-      //         var error = response.body;
-      //         should.exist(error);
+              var error = response.body;
+              should.exist(error);
 
-      //         var status = response.status;
-      //         status.should.equal(403);
+              var status = response.status;
+              status.should.equal(403);
 
-      //         var errorMessage = testUtils.getServerErrorMessage(response);
+              var errorMessage = testUtils.getServerErrorMessage(response);
 
-      //         should.exist(errorMessage);
-      //         errorMessage.should.equal('Access Denied');
+              should.exist(errorMessage);
+              errorMessage.should.equal('Access Denied');
 
-      //         done();
-      //       });
-      //   });
-      // });
+              done();
+            });
+        });
+      });
+
+      describe('when trying to get applications for a project they are a member of', function() {
+        var email1 = 'testuser1@test.com';
+        var email2 = 'testuser2@test.com';
+        var password = 'password';
+        var user1 = null;
+        var user2 = null;
+        var auth = null;
+        var project = null;
+        var projectApplication = null;
+
+        before(function(done) {
+          async.series([
+            function createUser1_step(cb) {
+              userService.createUsingCredentials({
+                email: email1,
+                password: password
+              }, function(err, _user) {
+                if (err) return cb(err);
+
+                user1 = _user;
+                cb();
+              });
+            },
+            function createUser2_step(cb) {
+              userService.createUsingCredentials({
+                email: email2,
+                password: password
+              }, function(err, _user) {
+                if (err) return cb(err);
+
+                user2 = _user;
+                cb();
+              });
+            },
+            function authenticateUser_step(cb) {
+              authService.authenticateCredentials({
+                email: email1,
+                password: password
+              }, function(err, _auth) {
+                if (err) return cb(err);
+
+                auth = _auth;
+                cb();
+              });
+            },
+            function createUserProjectStep_step(cb) {
+              projectService.create({
+                createdByUserId: user1._id,
+                name: 'Project 1',
+                shortDescription: 'short desc'
+              }, function(err, _project) {
+                if (err) return cb(err);
+
+                project = _project;
+                cb();
+              });
+            },
+            function createUserProjectApplications_step(cb) {
+              projectService.createApplication({
+                projectId: project._id,
+                userId: user2._id
+              }, function(err, _projectApplication) {
+                if (err) return cb(err);
+
+                projectApplication = _projectApplication;
+
+                cb();
+              });
+            }
+          ], done);
+        });
+
+        after(function(done) {
+          User.remove({
+            email: {
+              $in: [email1, email2]
+            }
+          }, done);
+        });
+
+        after(function(done) {
+          Auth.remove({
+            user: user1._id
+          }, done);
+        });
+
+        after(function(done) {
+          Project.remove({
+            _id: project._id
+          }, done);
+        });
+
+        after(function(done) {
+          ProjectApplication.remove({
+            _id: projectApplication._id
+          }, done);
+        });
+
+        it('should return a 403', function(done) {
+          agent
+            .get('/projects/' + project._id + '/applications')
+            .set('x-access-token', auth.token)
+            .end(function(err, response) {
+              should.not.exist(err);
+
+              var applications = response.body;
+              should.exist(applications);
+
+              applications.length.should.equal(1);
+
+              done();
+            });
+        });
+      });
     });
   });
 });
